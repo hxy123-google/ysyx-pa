3b37e512233714ab436e2f1dbda0052bf02267f0
惠鑫源 <huixinyuan06@126.com>
2024-12-06 09:31:23 -0500

    完成sprintf

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -4,8 +4,11 @@
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-
+static char NUM_CHAR[] = "0123456789ABCDEF";
 int printf(const char *fmt, ...) {
+  va_list ap;
+  va_start(ap,fmt);
+
   panic("Not implemented");
 }
 
@@ -14,7 +17,37 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  int len=0;
+  char buf[128];
+  va_list ap;
+  va_start(ap,fmt);
+  while(*fmt!='\0'){
+    if(*fmt=='%'){
+      fmt++;
+      if(*fmt=='d'){
+        int num_len=0;
+        int q=va_arg(ap,int);
+        if(q==0) out[len++]='0';
+        if(q<0) out[len++]='-';
+        for(;q!=0;q=q/10){
+          buf[num_len++]=NUM_CHAR[q%10];
+        }
+        for(int i=num_len-1;i>=0;i--){
+          out[len++]=buf[i];
+        }
+      }
+      else if(*fmt=='s'){
+        char* s = va_arg(ap, char*);
+				for(int i = 0; s[i] != '\0'; i++) 
+						out[len++] = s[i];
+      }
+      fmt++;
+    }else{
+       out[len++]=*fmt++;
+    }
+  }
+  out[len]='\0';
+  return len;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
